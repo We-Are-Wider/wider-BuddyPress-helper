99fbe5acd4cf3679fe8c6fc7fdd8936ba0017e38
Jonny Allbut <Jonnyauk@users.noreply.github.com>
2014-12-09 11:05:20 +0000

    Update references.
    Wider not Tanc ;)

--- tanc-buddypress-helper.php
+++ tanc-buddypress-helper.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Tanc BuddyPress Helper
-Plugin URI: http://www.tancdesign.com
+Plugin Name: Wider BuddyPress Helper
+Plugin URI: http://wider.co.uk
 Description: WORK IN PROGRESS! Adds useful BuddyPress global variables to the admin bar for debug purposes.
 Version: 0.2
-Author: Jonny Allbut/Tanc Design
+Author: Jonny Allbut/Wider
 Author URI: http://jonnya.net
 License: GPLv2 or later
 */
@@ -15,18 +15,18 @@ TODO: This is a good start, but needs to be extended to include all BuddyPress v
 
 // Only do stuff if BuddyPress is active thanks!
 // Do everything, but not in the admin area thanks buddy
-function tanc_bphelper_do() {
+function wider_bphelper_do() {
 	if ( is_super_admin() && !is_admin() ) {
-		add_action( 'admin_bar_menu', 'tanc_bphelper_add' );
-		add_action( 'admin_bar_menu', 'tanc_bphelper_info' );
+		add_action( 'admin_bar_menu', 'wider_bphelper_add' );
+		add_action( 'admin_bar_menu', 'wider_bphelper_info' );
 	}	
 }
-add_action('bp_init', 'tanc_bphelper_do', 1);
+add_action('bp_init', 'wider_bphelper_do', 1);
 
 
-function tanc_bphelper_add( $admin_bar ) {
+function wider_bphelper_add( $admin_bar ) {
 	$admin_bar->add_menu( array(
-	    'id'    => 'tanc-bp-helper', 
+	    'id'    => 'wider-bp-helper', 
 	    'parent' => 'top-secondary',
 	    'title' => 'BP Info',
 	    'href'  => '#',
@@ -37,7 +37,7 @@ function tanc_bphelper_add( $admin_bar ) {
 }
 
 
-function tanc_bphelper_info($admin_bar) {
+function wider_bphelper_info($admin_bar) {
 	$bp_val = array();
 
 	if ( bp_current_component() && bp_is_user() ) {
@@ -65,8 +65,8 @@ function tanc_bphelper_info($admin_bar) {
 
 				foreach ($value as $k => $v) {
 					$admin_bar->add_menu( array(
-					    'id'    => 'tanc-bp-helper-'.$k, 
-					    'parent' => 'tanc-bp-helper',
+					    'id'    => 'wider-bp-helper-'.$k, 
+					    'parent' => 'wider-bp-helper',
 					    'title' => $key . '[' . $k . '] = ' . $v,
 					    'href'  => '#',
 					    'meta'  => array(
@@ -78,8 +78,8 @@ function tanc_bphelper_info($admin_bar) {
 			} else {
 				
 				$admin_bar->add_menu( array(
-				    'id'    => 'tanc-bp-helper-'.$key, 
-				    'parent' => 'tanc-bp-helper',
+				    'id'    => 'wider-bp-helper-'.$key, 
+				    'parent' => 'wider-bp-helper',
 				    'title' => ucfirst($key) . ' = ' . $value,
 				    'href'  => '#',
 				    'meta'  => array(
@@ -93,8 +93,8 @@ function tanc_bphelper_info($admin_bar) {
 	
 	else
 		$admin_bar->add_menu( array(
-	    'id'    => 'tanc-bp-helper-2', 
-	    'parent' => 'tanc-bp-helper',
+	    'id'    => 'wider-bp-helper-2', 
+	    'parent' => 'wider-bp-helper',
 	    'title' => __('No informaion available'),
 	    'href'  => '#',
 	    'meta'  => array(
